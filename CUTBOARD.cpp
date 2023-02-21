9497d407085b2115e820494037ab15a8328d11e5
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-02-21 23:15:55 +0530

    Add files via upload

---
+++ CUTBOARD.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes 
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int n,m;
+	    cin>>n>>m;
+	    cout<<(m-1) * (n-1) <<endl;
+	}
+	return 0;
+}
