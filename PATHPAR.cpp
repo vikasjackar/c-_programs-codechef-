091fe3004f9d0e34c91d61d1c52b87f5c476ecc5
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-02-26 22:51:27 +0530

    Add files via upload

---
+++ PATHPAR.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    if(n%2==0)
+	    cout<<"Yes"<<endl;
+	    else
+	    {
+	        if(k==1)
+	        cout<<"Yes"<<endl;
+	        else
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
