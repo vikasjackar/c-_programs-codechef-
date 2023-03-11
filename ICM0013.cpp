2ff4269308ab19366107a2ddeb4cb67002dbc9fa
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-03-11 23:27:36 +0530

    Add files via upload

---
+++ ICM0013.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int count=0;
+	    if(n==1)
+	    count=0;
+	    else
+	    count=n;
+	    cout<<count<<endl;
+	}
+	return 0;
+}
