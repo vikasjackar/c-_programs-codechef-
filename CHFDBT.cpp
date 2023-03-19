f62c464e82aca4f481353d6ce2060cfc444ff6b6
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-03-19 20:01:07 +0530

    Add files via upload

---
+++ CHFDBT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    double n;
+	    cin>>n;
+	    int temp=ceil(n/2);
+	    cout<<temp<<endl;
+	}
+	return 0;
+}
