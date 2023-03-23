ec123c003f47a3d9216fb9970b5679f5f37aaee4
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-03-23 22:56:30 +0530

    Add files via upload

---
+++ REACH_HOME.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    if(5*x>=y)
+	    {
+	        cout<<"Yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
