1a9c7d09068cff3b76c4243af5bd0e21efd42a8c
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-04-05 23:48:59 +0530

    Add files via upload

---
+++ DNATION.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(y>x)
+	    {
+	        cout<<y-x<<endl;
+	    }
+	}
+	return 0;
+}
