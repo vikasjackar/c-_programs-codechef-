212728e22dc58743a1a8bbce2c89eb1e1ef3e929
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-04-09 23:05:18 +0530

    Add files via upload

---
+++ MAX_BIN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {// your code goes here
+	int t,n,k,i;	string s;	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>k;    cin>>s;
+	    if(s[0]=='0')
+	    {
+	        s[0]='1';    k--;
+	    }
+	    cout<<s;
+	    while(k--)
+	        cout<< 0;
+	    cout<<endl;
+	}
+	return 0;
+}
