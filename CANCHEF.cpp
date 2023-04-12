ee286828cd36b6d6fcfa44cb9c937cc2fbed0dfd
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-04-12 23:59:10 +0530

    Add files via upload

---
+++ CANCHEF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(15*x>=2*y){
+            cout<<"YES"<<endl;
+        }
+        else{
+             cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
