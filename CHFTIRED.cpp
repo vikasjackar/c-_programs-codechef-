b0f25ced95cc062434a7f5cec680045d5a9b1d3a
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-05-10 22:17:25 +0530

    Add files via upload

---
+++ CHFTIRED.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define l long long int 
+int main(){
+    l t;
+    cin>>t;
+    while(t--){
+        l a,b;
+        cin>>a>>b;
+        cout<<"YES"<<endl;
+    }
+}
