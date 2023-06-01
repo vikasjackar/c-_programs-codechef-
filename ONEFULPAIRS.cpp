488f677a0320252517748978481409967a79555e
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-06-01 22:36:20 +0530

    Add files via upload

---
+++ ONEFULPAIRS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a+b+(a*b)==111){
+	        cout<<"YES"<<endl;
+	}
+	else{
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
