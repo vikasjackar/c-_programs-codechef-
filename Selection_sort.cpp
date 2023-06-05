6151b0612c50c5ca09d18d5f7a2dea3bdce3a864
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-06-05 11:49:28 +0530

    Create Selection_sort.cpp

---
+++ Selection_sort.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+void selectionsort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int min_index=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[min_index])
+            {
+                min_index=j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    selectionsort(arr,n);
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
