3ef927f6ba3caa25f45654fbc166aada6543ac7b
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-06-08 23:17:15 +0530

    Add files via upload

---
+++ RIGHTTHERE.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    if(n<=x)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
