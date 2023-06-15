b264f7360115897a0f945b1373a6aae71ca1a116
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-06-15 23:52:19 +0530

    Add files via upload

---
+++ CS2023_GIFT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,n,m;
+	cin>>x>>n>>m;
+	if((x+m)>=n){
+	    cout<<"yes"<<endl;
+
+	}else{
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
