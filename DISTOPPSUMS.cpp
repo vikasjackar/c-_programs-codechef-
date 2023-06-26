834c538c3e8a1fe622802b8d85f1d1e2694077cf
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-06-26 23:38:28 +0530

    Add files via upload

---
+++ DISTOPPSUMS.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ll           long long
+#define PI           3.1415926535897932384626
+#define rep(i,n)     for(int i=0;i<n;i++)
+#define repk(i,k,n)  for(int i=k;i<n;i++)
+#define pb           push_back
+#define vec          vector<int>
+#define u_map        unordered_map<int,int>
+#define vecp         vector<pair<int,int>>
+#define ff           first 
+#define ss           second
+#define yes          cout << "YES" << endl;
+#define cout_ans     cout<<ans<<endl;
+#define no           cout << "NO" << endl;
+#define all(v)       (v.begin),b.end();
+#define sortall(v)   sort(all(v));
+
+/*****************************#define*************************/
+
+const int N = 1e5;
+const int INF = int(1e9) + 99;
+const int MAXI = 20001;
+const int MAXN = 1e5 + 4;
+const int MOD1 = 998244353;
+ 
+/*******************************cons****************************/
+
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+
+void solve(){ 
+     int a,b;
+     cin>>a;
+
+     int i=1;
+
+     while(i<=a/2)
+     {
+        cout<<i<<" ";
+        i++;
+     }
+     i=a;
+
+     while(i>a/2)
+     {
+        cout<<i<<" ";
+        i--;
+     }
+     cout<<endl;
+      
+     
+}
+
+
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        solve();
+      
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
