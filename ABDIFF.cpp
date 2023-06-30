36fcb829fdaa429d5946057460bf5e40aac594fb
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-06-30 23:21:52 +0530

    Add files via upload

---
+++ ABDIFF.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+#include<math.h>
+#include<string.h> 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int a,b; cin>>a>>b;
+    cout<<abs((a*b)-(a+b))<<endl;
+    
+    return 0;
+    
+}
