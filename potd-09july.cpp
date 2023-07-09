82bb3b0c6cee45f26bfffcad8f161ff39986c707
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-07-09 22:41:49 +0530

    Create potd-09july.cpp

---
+++ potd-09july.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        int track = 1 ;
+        sort(arr,arr+n) ;
+        for(int i = 0 ; i<n;i++){
+            if(arr[i]>0){
+                if(i==0 && arr[i]==track)
+                    track++ ;
+                else if(i>0 && arr[i]!=arr[i-1]&& arr[i]==track) 
+                    track++ ;
+                else if(i>0 && arr[i]==arr[i-1]) 
+                    continue ;
+                else 
+                    return track ;
+            }
+        }
+        return track ;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int missingNumber(int arr[], int n);
+
+int main() { 
+    
+    //taking testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //input number n
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        //adding elements to the array
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        Solution ob;
+        //calling missingNumber()
+        cout<<ob.missingNumber(arr, n)<<endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
