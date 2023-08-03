9bdad8882dd6d9630f319839ddc0e90cf26ec5a6
Vikas Kumar Verma <vikasjackar@gmail.com>
2023-08-03 10:36:10 +0530

    Create potd-03august.cpp

---
+++ potd-03august.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edge){
+        // code here
+         vector<pair<int,int>>adj[N];
+        for(int i=0;i<M;i++){
+            adj[edge[i][0]].push_back({edge[i][1],edge[i][2]});
+        }
+        vector<int>dist(N,1e9);
+        dist[0]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,0});
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            for(auto it:adj[node]){
+                int adjnode=it.first;
+                int edw=it.second;
+                if(dis+edw<dist[adjnode]){
+                    dist[adjnode]=dis+edw;
+                    pq.push({dist[adjnode],adjnode});
+                }
+            }
+        }
+        for(int i=0;i<dist.size();i++){
+            if(dist[i]==1e9){
+                dist[i]=-1;
+            }
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
